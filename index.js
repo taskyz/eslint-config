262cdd4ad9b0a4d0ddce7b43cb8cda9127372844
Faris CHTATOU <faris.manage@gmail.com>
2024-01-13 16:31:25 +0100

    update rules

--- index.js
+++ index.js
@@ -7,6 +7,9 @@ module.exports = {
     "plugin:import/warnings",
   ],
   rules: {
+    "@typescript-eslint/interface-name-prefix": "off",
+    "@typescript-eslint/explicit-function-return-type": "off",
+    "@typescript-eslint/explicit-module-boundary-types": "off",
     "@typescript-eslint/no-explicit-any": "warn",
     "no-console": "error",
     "import/no-unresolved": "off",
