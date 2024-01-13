ee0d0cebda1f8d85a120e64d16abba71cd92a36b
Faris CHTATOU <faris.manage@gmail.com>
2024-01-13 15:55:47 +0100

    first commit

---
+++ index.js
@@ -0,0 +1,64 @@
+module.exports = {
+  plugins: ["@typescript-eslint/eslint-plugin", "import"],
+  extends: [
+    "plugin:@typescript-eslint/recommended",
+    "plugin:prettier/recommended",
+    "plugin:import/errors",
+    "plugin:import/warnings",
+  ],
+  rules: {
+    "@typescript-eslint/no-explicit-any": "on",
+    "no-console": "error",
+    "import/no-unresolved": "off",
+    "import/no-extraneous-dependencies": [
+      "error",
+      {
+        devDependencies: true,
+        peerDependencies: true,
+      },
+    ],
+    "no-multiple-empty-lines": [
+      "error",
+      {
+        max: 1,
+        maxEOF: 0,
+        maxBOF: 0,
+      },
+    ],
+    "@typescript-eslint/no-unused-vars": [
+      "error",
+      {
+        varsIgnorePattern: "_",
+      },
+    ],
+    "import/order": [
+      "error",
+      {
+        groups: [
+          ["external", "builtin"],
+          ["internal"],
+          ["parent", "index", "sibling"],
+        ],
+        pathGroups: [
+          {
+            pattern: "@/**",
+            group: "internal",
+          },
+          {
+            pattern: "./**",
+            group: "internal",
+          },
+          {
+            pattern: "../**",
+            group: "internal",
+          },
+        ],
+        "newlines-between": "always",
+        alphabetize: {
+          order: "asc",
+          caseInsensitive: true,
+        },
+      },
+    ],
+  },
+};
