8a1b28a17a54440313db2298f39b710a20259c52
Faris CHTATOU <faris.manage@gmail.com>
2024-01-25 20:54:14 +0100

    add prettier configuration

--- index.js
+++ index.js
@@ -3,76 +3,72 @@ module.exports = {
   env: {
     node: true,
   },
-  plugins: ["@typescript-eslint/eslint-plugin", "import"],
+  plugins: ['@typescript-eslint/eslint-plugin', 'import'],
   extends: [
-    "eslint:recommended",
-    "plugin:@typescript-eslint/recommended",
-    "plugin:prettier/recommended",
-    "plugin:import/errors",
-    "plugin:import/warnings",
+    'eslint:recommended',
+    'plugin:@typescript-eslint/recommended',
+    'plugin:prettier/recommended',
+    'plugin:import/errors',
+    'plugin:import/warnings',
   ],
   rules: {
-    "@typescript-eslint/interface-name-prefix": "off",
-    "@typescript-eslint/explicit-function-return-type": "off",
-    "@typescript-eslint/explicit-module-boundary-types": "off",
-    "@typescript-eslint/no-explicit-any": "error",
-    "@typescript-eslint/ban-ts-comment": [
-      "error",
+    '@typescript-eslint/interface-name-prefix': 'off',
+    '@typescript-eslint/explicit-function-return-type': 'off',
+    '@typescript-eslint/explicit-module-boundary-types': 'off',
+    '@typescript-eslint/no-explicit-any': 'error',
+    '@typescript-eslint/ban-ts-comment': [
+      'error',
       {
-        "ts-expect-error": { descriptionFormat: "^: TS\\d+ because .+$" },
-        "ts-ignore": true,
-        "ts-nocheck": true,
-        "ts-check": false,
+        'ts-expect-error': { descriptionFormat: '^: TS\\d+ because .+$' },
+        'ts-ignore': true,
+        'ts-nocheck': true,
+        'ts-check': false,
       },
     ],
-    "no-console": "error",
-    "import/no-unresolved": "off",
-    "import/no-extraneous-dependencies": [
-      "error",
+    'no-console': 'error',
+    'import/no-unresolved': 'off',
+    'import/no-extraneous-dependencies': [
+      'error',
       {
         devDependencies: true,
         peerDependencies: true,
       },
     ],
-    "no-multiple-empty-lines": [
-      "error",
+    'no-multiple-empty-lines': [
+      'error',
       {
         max: 1,
         maxEOF: 0,
         maxBOF: 0,
       },
     ],
-    "@typescript-eslint/no-unused-vars": [
-      "error",
+    '@typescript-eslint/no-unused-vars': [
+      'error',
       {
-        varsIgnorePattern: "_",
+        varsIgnorePattern: '_',
       },
     ],
-    "import/order": [
-      "error",
+    'import/order': [
+      'error',
       {
-        groups: [
-          ["external", "builtin"],
-          ["internal"],
-          ["parent", "index", "sibling"],
-        ],
+        groups: [['external', 'builtin'], ['internal'], ['parent', 'index', 'sibling']],
         pathGroups: [
           {
-            pattern: "@/**",
-            group: "internal",
+            pattern: '@/**',
+            group: 'internal',
           },
           {
-            pattern: "./**",
-            group: "internal",
+            pattern: './**',
+            group: 'internal',
           },
           {
-            pattern: "../**",
-            group: "internal",
+            pattern: '../**',
+            group: 'internal',
           },
         ],
-        "newlines-between": "always",
+        'newlines-between': 'always',
         alphabetize: {
-          order: "asc",
+          order: 'asc',
           caseInsensitive: true,
         },
       },
